6165a21bdca75eb8a147060c0bdeab488b192781
ug00031 <ug00031@surrey.ac.uk>
2021-03-19 21:46:50 +0000

    removed env

--- plugins/actioncable.ts
+++ plugins/actioncable.ts
@@ -4,7 +4,7 @@ import ActionCableVue from 'actioncable-vue';
 Vue.use(ActionCableVue, {
     debug: true,
     debugLevel: 'error',
-    connectionUrl: process.env.WEBSOCKET_URL || 'ws://localhost:3000/news',
+    connectionUrl: 'wss://crime-spotter-backend.herokuapp.com/news',
     connectImmediately: true,
   });
   
