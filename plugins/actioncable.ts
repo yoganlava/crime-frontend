7005cd21fc70fb9ed0644f510efdce8156dc5522
ug00031 <ug00031@surrey.ac.uk>
2021-03-19 21:30:05 +0000

    support for websocket env var

--- plugins/actioncable.ts
+++ plugins/actioncable.ts
@@ -4,8 +4,7 @@ import ActionCableVue from 'actioncable-vue';
 Vue.use(ActionCableVue, {
     debug: true,
     debugLevel: 'error',
-    // connectionUrl: 'wss://crime-spotter-backend.herokuapp.com/news',
-    connectionUrl: 'ws://localhost:3000/news',
+    connectionUrl: process.env.WEBSOCKET_URL || 'ws://localhost:3000/news',
     connectImmediately: true,
   });
   
