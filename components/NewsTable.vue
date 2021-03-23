d4a973049f4510d133f591398f4d20c9714f216c
ug00031 <ug00031@surrey.ac.uk>
2021-03-23 23:05:35 +0000

    added news pagination

--- components/NewsTable.vue
+++ components/NewsTable.vue
@@ -3,9 +3,28 @@
     class="bg-white border-2 border-gray-300 p-3 rounded-md tracking-wide shadow-lg news-table"
   >
     <h1 class="table-title">Live news in London</h1>
-    <div v-for="news, i in currentNews" :key="i">
-        <news-article :title="news.title" :description="news.description" :url="news.url"></news-article>
+    <div v-for="news, i in paginatedNews()" :key="i">
+      <news-article
+        :title="news.title"
+        :description="news.description"
+        :url="news.url"
+      ></news-article>
     </div>
+    <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+        <a @click="decrementPageNumber" class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+          </svg>
+        </a>
+        <p class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
+          {{pageNum}}
+        </p>
+        <a @click="incrementPageNumber"  class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+          </svg>
+        </a>
+    </nav>
   </div>
 </template>
 
@@ -24,6 +43,8 @@ export default class NewsTable extends Vue {
     }
   };
   currentNews: Array<any> = [];
+  pageNum: number = 1;
+  limit: number = 5;
 
   mounted() {
     this.$cable.subscribe({
@@ -79,7 +100,22 @@ export default class NewsTable extends Vue {
       o1.url === o2.url
     );
   }
-}8
+
+  incrementPageNumber() {
+    if (this.pageNum * this.limit < this.currentNews.length)
+      this.pageNum++;
+  }
+
+  decrementPageNumber() {
+    if (this.pageNum > 1)
+      this.pageNum--;
+  }
+
+  paginatedNews() {
+    return this.currentNews.slice((this.pageNum - 1) * this.limit, this.pageNum * this.limit);
+  }
+}
+8;
 </script>
 
 <style>
