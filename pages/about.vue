e45696db8864943b761b0942f80d6014ddaf1ded
Rafael Koullouros <r.koullouros@gmail.com>
2021-04-15 16:22:12 +0300

    Added information on about page.

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,37 @@
 <template>
-  <div></div>
+  <div>
+    <div class="flex justify-center mt-8 flex-wrap">
+      <h1
+        class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate"
+      >
+        About
+      </h1>
+
+    </div>
+    <div class="mt-2 mx-auto" style="width: 50%;">
+      <p class="text-center">
+        This is a free tool that provides a way to look at crime in different locations.
+        This can be used for a plethora of purposes, such as looking at the crime rate of a
+        location that you plan on moving to. Simply by entering a location, you can view
+        the crimes at that location. 
+      </p>
+    </div>
+    <div class="flex mt-8 mx-auto justify-center" style="width: 50%;">
+      <h2
+        class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xm sm:truncate"
+      >
+      Credits
+      </h2>
+    </div>
+    <div class="flex mt-2 mx-auto justify-center">
+      <ul class="text-center">
+        <li>Matthew McLoughlin</li>
+        <li>Rafael Koullouros</li>
+        <li>Udeshya Gurung</li>
+        <li>Theo Sweet</li>
+        <li>Connor Nolan</li>
+        <li>Blake Fowler</li>
+      </ul>
+    </div>
+  </div>
 </template>
