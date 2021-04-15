96152d715c976a7ce7039d5d567eed66d1b9f36b
Rafael Koullouros <r.koullouros@gmail.com>
2021-04-15 16:55:51 +0300

    modified about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
-    <div class="flex justify-center mt-8 flex-wrap">
-      <h1
-        class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate"
-      >
+    <div class="flex justify-center mt-8 mb-2 flex-wrap">
+      <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
         About
       </h1>
 
     </div>
-    <div class="mt-2 mx-auto" style="width: 50%;">
+    <div class=" mx-auto" style="width: 50%;">
       <p class="text-center">
         This is a free tool that provides a way to look at crime in different locations.
-        This can be used for a plethora of purposes, such as looking at the crime rate of a
-        location that you plan on moving to. Simply by entering a location, you can view
-        the crimes at that location. 
+        This information can be used for a plethora of purposes by many people, such as looking at crime
+        rates before visiting a certain location as a tourist, home owners looking who want to choose a 
+        security system for their homes, people moving to a new location, or any one else!<br>
+        The ease of use also makes it perfect for anyone to quickly look up the information they need, such as
+        elderly people.
       </p>
     </div>
-    <div class="flex mt-8 mx-auto justify-center" style="width: 50%;">
-      <h2
-        class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xm sm:truncate"
-      >
+
+    <div class="flex mt-8 mb-2 mx-auto justify-center" style="width: 60%;">
+      <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xm sm:truncate">
       Credits
       </h2>
     </div>
-    <div class="flex mt-2 mx-auto justify-center">
+
+    <div class="flex mx-auto justify-center">
       <ul class="text-center">
         <li>Matthew McLoughlin</li>
         <li>Rafael Koullouros</li>
@@ -34,4 +34,6 @@
       </ul>
     </div>
   </div>
+
+
 </template>
